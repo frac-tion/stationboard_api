715424d4ed76cd5802f8a1f919cc7d56c4d8a70d
Julian Sparber <julian@sparber.net>
2015-10-08 00:07:53 +0200

    [feat] SAD trains returns all trains delay

--- realtimeSAD.js
+++ realtimeSAD.js
@@ -1,6 +1,6 @@
 var logLevel = 'debug';
 
-var SAD_API = "http://www.sad.it/ferrovia/index.php?page=mappe.mabo"
+var SAD_API = "http://www.sad.it/ferrovia/index.php?page=";
 
 /*
  * Log level:
@@ -25,30 +25,34 @@ var log = new Log(logLevel);
 
 realtimeSad();
 function realtimeSad(stationId, dateTime, callback) {
-    request({url: SAD_API,
-      gzip: true,
-      headers: {
-        'Connection': 'keep-alive',
-        'Accept-Encoding': 'gzip, deflate'
-      }
-    },
-    function(err, res, body) {
-      if(!err) {
-        if (res.statusCode === 200) {
-          //log.debug(body);
-          //trainDelay(body, callback);
-          //trainDelays(el.codOrigine + "/" + el.numeroTreno);
-          //if(callback)
-          //  callback(parseStation(body));
-          var $ = cheerio.load(body);
-          var treno = $('.treno');
-          while (treno.length > 0) {
+	var apiList = ["mappe.mabo", "mappe.bzbrca&mappa=2_1", "mappe.bzbrca&mappa=2_2"];
+	var resList = {};
+	apiList.forEach(function (map) {
+		request({url: SAD_API + map,
+			gzip: true,
+			headers: {
+				'Connection': 'keep-alive',
+				'Accept-Encoding': 'gzip, deflate'
+			}
+		},
+		function(err, res, body) {
+			if(!err) {
+				if (res.statusCode === 200) {
+					//log.debug(body);
+					//trainDelay(body, callback);
+					//trainDelays(el.codOrigine + "/" + el.numeroTreno);
+					//if(callback)
+					//  callback(parseStation(body));
+					var $ = cheerio.load(body);
+					var treno = $('.treno');
+					while (treno.length > 0) {
 						var curr = treno.first().text();
-						
+						var res = {};
+
 						//search for delay
-            var delayPos = curr.indexOf('+');
+						var delayPos = curr.indexOf('+');
 						if (delayPos == -1)
-            	delayPos = curr.indexOf('- ');
+							delayPos = curr.indexOf('- ');
 
 						//search end of delay
 						if (delayPos != -1) {
@@ -56,33 +60,27 @@ function realtimeSad(stationId, dateTime, callback) {
 						}
 
 						var numberPos = curr.indexOf(String.fromCharCode(160));
-						console.log(curr.slice(numberPos + 1, curr.indexOf(String.fromCharCode(160), numberPos + 1)));
+						res.number = curr.slice(numberPos + 1, curr.indexOf(String.fromCharCode(160), numberPos + 1));
 						var currDest = curr.replace(/  |\n/gi, "");
 						var indexDest = currDest.lastIndexOf(String.fromCharCode(160));
-						console.log(currDest.slice(indexDest + 2));
-						if (delay != undefined)
-            	console.log(delay);
+						res.destination = currDest.slice(indexDest + 2).replace(/Staz./gi, 'Stazione');
+						//log.debug(train.numeroTreno + " has a delay of " + train.ritardo + " min");
+						//res.departure = (new Date(train.orarioPartenza)).getTime();
+						res.delay = delay;
 
-            treno = treno.next()
-          }
-        }
-        else
-          log.error("StatuCode: " + res.statusCode);
-      }
-      else
-        log.error(err);
-    });
+						resList[res.number] = res;
+						console.log(resList);
+
+						treno = treno.next()
+					}
+				}
+				else
+					log.error("StatuCode: " + res.statusCode);
+			}
+			else
+				log.error(err);
+		});
+	});
 }
-
-function parseTrain(train) {
-  var res = {};
-    log.debug(train.numeroTreno + " has a delay of " + train.ritardo + " min");
-    res.delay = train.ritardo;
-    res.number = train.numeroTreno;
-    res.destination = train.destinazione;
-    res.departure = (new Date(train.orarioPartenza)).getTime();
-  return res;
-}
-
 
 module.exports = realtimeSad;
