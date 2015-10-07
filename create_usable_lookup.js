3c39933d81d308a6ee44252f5969f64b2885057d
Julian Sparber <julian@sparber.net>
2015-10-07 12:55:22 +0200

    [feat] create
    lookup table for trenitalia stations

---
+++ create_usable_lookup.js
@@ -0,0 +1,257 @@
+var data = {
+	"S05043": {
+		"nameTrenitalia": "BOLOGNA C.LE",
+		"idSii": "66007011",
+		"nameSii": "Bologna C.le Staz.Bologna"
+	},
+	"S05302": {
+		"nameTrenitalia": "OSTERIA NUOVA",
+		"idSii": "",
+		"nameSii": ""
+	},
+	"S05319": {
+		"nameTrenitalia": "SAN GIOVANNI IN PERSICETO",
+		"idSii": "66007019",
+		"nameSii": "San Giovanni Persiceto Staz.Bologna"
+	},
+	"S05316": {
+		"nameTrenitalia": "CREVALCORE",
+		"idSii": "66007018",
+		"nameSii": "Crevalcore Staz.Crevalcore"
+	},
+	"S05313": {
+		"nameTrenitalia": "S.FELICE SP",
+		"idSii": "66007017",
+		"nameSii": "S.Felice sul Panaro Staz.S.Felice sul Panaro"
+	},
+	"S05311": {
+		"nameTrenitalia": "MIRANDOLA",
+		"idSii": "66000301",
+		"nameSii": "Stazione Maia BassaMerano"
+	},
+	"S05308": {
+		"nameTrenitalia": "POGGIO RUSCO",
+		"idSii": "66007016",
+		"nameSii": "Poggio Rusco Staz.Poggio Rusco"
+	},
+	"S05304": {
+		"nameTrenitalia": "OSTIGLIA",
+		"idSii": "66007015",
+		"nameSii": "Ostiglia Staz.Ostiglia"
+	},
+	"S05310": {
+		"nameTrenitalia": "NOGARA",
+		"idSii": "66007014",
+		"nameSii": "Nogara Staz., Nogara"
+	},
+	"S05307": {
+		"nameTrenitalia": "ISOLA DELLA SCALA",
+		"idSii": "66007013",
+		"nameSii": "Isola della Scala Staz.Isola della Scala"
+	},
+	"S02430": {
+		"nameTrenitalia": "VERONA PORTA NUOVA",
+		"idSii": "66007010",
+		"nameSii": "Verona Porta NuovaVerona"
+	},
+	"S02055": {
+		"nameTrenitalia": "DOMEGLIARA",
+		"idSii": "66007009",
+		"nameSii": "DomegliaraDomegliara"
+	},
+	"S02049": {
+		"nameTrenitalia": "ALA",
+		"idSii": "66007005",
+		"nameSii": "Ala, Ala (p Trento)"
+	},
+	"S02044": {
+		"nameTrenitalia": "ROVERETO",
+		"idSii": "66007002",
+		"nameSii": "Rovereto, Rovereto (p Trento)"
+	},
+	"S02038": {
+		"nameTrenitalia": "TRENTO",
+		"idSii": "66000662",
+		"nameSii": "StazioneTrento"
+	},
+	"S02035": {
+		"nameTrenitalia": "MEZZOCORONA",
+		"idSii": "66007001",
+		"nameSii": "MezzocoronaMezzocorona"
+	},
+	"S02031": {
+		"nameTrenitalia": "ORA",
+		"idSii": "66000647",
+		"nameSii": ""
+	},
+	"S02026": {
+		"nameTrenitalia": "BOLZANO",
+		"idSii": "66000468",
+		"nameSii": ""
+	},
+	"S02020": {
+		"nameTrenitalia": "PONTE GARDENA",
+		"idSii": "66001092",
+		"nameSii": ""
+	},
+	"S02017": {
+		"nameTrenitalia": "CHIUSA",
+		"idSii": "66001051",
+		"nameSii": "StazioneChiusa"
+	},
+	"S02014": {
+		"nameTrenitalia": "BRESSANONE",
+		"idSii": "66000998",
+		"nameSii": "StazioneBressanone"
+	},
+	"S02011": {
+		"nameTrenitalia": "FORTEZZA",
+		"idSii": "66001170",
+		"nameSii": "StazioneFortezza"
+	},
+	"S02007": {
+		"nameTrenitalia": "CAMPO DI TRENS",
+		"idSii": "66001492",
+		"nameSii": "StazioneCampo di Trens"
+	},
+	"S02006": {
+		"nameTrenitalia": "VIPITENO",
+		"idSii": "66001393",
+		"nameSii": "Stazione Vipiteno - Val di VizzeVal di Vizze"
+	},
+	"S02005": {
+		"nameTrenitalia": "COLLE ISARCO",
+		"idSii": "66001385",
+		"nameSii": ""
+	},
+	"S02001": {
+		"nameTrenitalia": "BRENNERO",
+		"idSii": "66001389",
+		"nameSii": "StazioneBrennero"
+	},
+	"S02029": {
+		"nameTrenitalia": "LAIVES",
+		"idSii": "66002444",
+		"nameSii": "StazioneLaives"
+	},
+	"S02030": {
+		"nameTrenitalia": "BRONZOLO",
+		"idSii": "66002460",
+		"nameSii": ""
+	},
+	"S02032": {
+		"nameTrenitalia": "EGNA-TERMENO",
+		"idSii": "66000696",
+		"nameSii": "Stazione Egna - TermenoTermeno sulla strada del vino"
+	},
+	"S02033": {
+		"nameTrenitalia": "MAGRE`- CORTACCIA",
+		"idSii": "66000679",
+		"nameSii": "Stazione Magrè - CortacciaMagre' sulla strada del vino"
+	},
+	"S02034": {
+		"nameTrenitalia": "SALORNO",
+		"idSii": "66000663",
+		"nameSii": "StazioneSalorno"
+	},
+	"S02037": {
+		"nameTrenitalia": "LAVIS",
+		"idSii": "66000661",
+		"nameSii": ""
+	},
+	"S08203": {
+		"nameTrenitalia": "ORVIETO",
+		"idSii": "",
+		"nameSii": ""
+	},
+	"S08409": {
+		"nameTrenitalia": "ROMA TERMINI",
+		"idSii": "",
+		"nameSii": ""
+	},
+	"S02226": {
+		"nameTrenitalia": "BOLZANO SUD",
+		"idSii": "66002210",
+		"nameSii": ""
+	},
+	"S02059": {
+		"nameTrenitalia": "CASANOVA KAISERAU",
+		"idSii": "66002132",
+		"nameSii": ""
+	},
+	"S02225": {
+		"nameTrenitalia": "PONTE D`ADIGE",
+		"idSii": "66000811",
+		"nameSii": ""
+	},
+	"S02224": {
+		"nameTrenitalia": "SETTEQUERCE",
+		"idSii": "66000458",
+		"nameSii": ""
+	},
+	"S02223": {
+		"nameTrenitalia": "TERLANO-ANDRIANO",
+		"idSii": "66000454",
+		"nameSii": ""
+	},
+	"S02222": {
+		"nameTrenitalia": "VILPIANO-NALLES",
+		"idSii": "66002424",
+		"nameSii": "Stazione Vilpiano - NallesTerlano"
+	},
+	"S02218": {
+		"nameTrenitalia": "GARGAZZONE",
+		"idSii": "66002421",
+		"nameSii": "StazioneGargazzone"
+	},
+	"S02219": {
+		"nameTrenitalia": "LANA-POSTAL",
+		"idSii": "66002294",
+		"nameSii": "Stazione Lana - PostalPostal"
+	},
+	"S02217": {
+		"nameTrenitalia": "MERANO MAIA BASSA",
+		"idSii": "66000301",
+		"nameSii": "Stazione Maia BassaMerano"
+	},
+	"S02216": {
+		"nameTrenitalia": "MERANO",
+		"idSii": "66000210",
+		"nameSii": ""
+	},
+	"S02046": {
+		"nameTrenitalia": "MORI",
+		"idSii": "66007003",
+		"nameSii": ""
+	},
+	"S02048": {
+		"nameTrenitalia": "SERRAVALLE",
+		"idSii": "",
+		"nameSii": ""
+	},
+	"S02050": {
+		"nameTrenitalia": "AVIO",
+		"idSii": "66007006",
+		"nameSii": ""
+	},
+	"S02051": {
+		"nameTrenitalia": "BORGHETTO",
+		"idSii": "",
+		"nameSii": ""
+	},
+	"S02052": {
+		"nameTrenitalia": "PERI",
+		"idSii": "",
+		"nameSii": ""
+	},
+	"S02053": {
+		"nameTrenitalia": "POSTO COMUNICAZIONE DOLCE`",
+		"idSii": "",
+		"nameSii": ""
+	}
+}
+var res = {};
+for (var el in data) {
+	res[data[el].idSii] = el;
+}
+console.log(res);
